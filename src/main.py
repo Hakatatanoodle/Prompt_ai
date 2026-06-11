b92252e2c4139757cedf84e34d5da5c1a5441219
Hakata <literallyme098@gmail.com>
2026-06-11 20:55:32 +0545

    accepting post json request this will be used for receiving the message (prompt) from front end to backend

--- src/main.py
+++ src/main.py
@@ -2,10 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+#decorator - connects a url path to a funciton 
+@app.route('/chat',methods = ['POST'])#methods is used to explicitely tell flask to accept POST
 
-def hello():
-    return "Hello world"
+
+
+def POST():
+    data  = request.get_json()
+
+    return "Message received !"
 
 if __name__ == '__main__':
     app.run()
+
+
+
+
+#NOTES 
+# 1. By default Flask routes only accept GET so you have to explicitly tell it to accept POST.
