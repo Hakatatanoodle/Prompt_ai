0e5e74bc29cba31ddda58096f0f2fb07b2cc6204
Hakata <literallyme098@gmail.com>
2026-06-12 21:03:56 +0545

    POST request to backend working

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
 from flask import Flask 
+from flask import request
 
 app = Flask(__name__)
 
 #decorator - connects a url path to a funciton 
 @app.route('/chat',methods = ['POST'])#methods is used to explicitely tell flask to accept POST
-
-
-
-def POST():
+def chat():
     data  = request.get_json()
-
+    print(data)
+    print(data["message"])
     return "Message received !"
 
 if __name__ == '__main__':
@@ -20,3 +19,5 @@ if __name__ == '__main__':
 
 #NOTES 
 # 1. By default Flask routes only accept GET so you have to explicitly tell it to accept POST.
+# 2. In Flask, anything created at the module level gets created once when the server starts and stays alive as long as the server is running.
+
