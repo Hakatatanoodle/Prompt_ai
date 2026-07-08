eed236ca402d366d4bdf1831bad6dc3d84320498
Hakata <literallyme098@gmail.com>
2026-07-08 21:32:16 +0545

    fixed ui and updated css worked on conversation and getting the final prompt made the page scroll to the place where the ai is responding

--- src/static/script.js
+++ src/static/script.js
@@ -1,14 +1,18 @@
+
+
 function sendMessage()
 {
     let prompt = document.getElementById("prompt");
-    document.getElementById("conversation").innerHTML+="<p>" + prompt.value+ "</p>";
+    if(prompt.value == "")
+    document.getElementById("conversation").innerHTML+="<p class = 'user_message'>" + prompt.value+ "</p>";
     fetch('/chat',{
         method:'POST',
         headers:{'Content-Type':'application/json'
         },
         body:JSON.stringify({message: prompt.value})})
     .then(response=>response.text())
-    .then(data=> document.getElementById("conversation").innerHTML += "<p>"+marked.parse(data)+"</p>")
+    .then(data=> document.getElementById("conversation").innerHTML += "<p class = 'ai_message'>"+marked.parse(data)+"</p>")
     document.getElementById("prompt").value = "";
+    window.scrollTo(0, document.body.scrollHeight);
 }
 
